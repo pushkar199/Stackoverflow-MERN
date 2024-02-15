5c7de5d32a5f098b61d1203e026e089708d696ad
pushkar199 <thaturpushkar99@gmail.com>
2024-02-15 23:21:27 +0530

    Adding middleware to logout route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const router = require("express").Router();
 const authController = require("../controller/auth");
+const auth = require( "../middleware/auth" );
 
 router.post('/api/register', authController.register);
 router.post('/api/login', authController.login);
-router.post('/api/logout', authController.logout);
+router.post('/api/logout', auth, authController.logout);
 
 module.exports = router;
